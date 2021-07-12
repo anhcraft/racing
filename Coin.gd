8e42cddd0f5e8fcc8731826ecc9547cdfeab4e0f
Duy Anh <huynhduyanh123123@gmail.com>
2021-07-12 10:40:17 +0700

    Increased normal coin bonus

--- Coin.gd
+++ Coin.gd
@@ -1,5 +1,13 @@
 extends Area2D
 
 func _on_Coin_body_entered(body):
-	$"/root/Player".deposit(1)
+	var pos = $"/root/Player".position.x;
+	var d = 1;
+	if pos > 500000:
+		d = 5;
+	elif pos > 200000:
+		d = 3;
+	elif pos > 80000:
+		d = 2;
+	$"/root/Player".deposit(d)
 	self.queue_free()
